a15e1bfeff913c35c6388e443dd256a2dd5a7146
Eric Zuniga <egz11912@gmail.com>
2021-01-14 16:47:08 -0700

    First submission for GitHub

---
+++ Gunstoreshoppingcart/Gunstoreshoppingcart/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Gunstoreshoppingcart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Eric ";
+            string lastName = "Zuniga";
+            string fullName = firstName + "" + lastName;
+            Console.WriteLine(fullName);
+
+            int x = 5;
+            int y = 6;
+            Console.WriteLine(x + y);
+
+            int sum1 = 10 + 5;        
+            int sum2 = sum1 + 100;      
+            int sum3 = sum2 + sum2;     
+            Console.WriteLine(sum3);
+        }
+    }
+}
