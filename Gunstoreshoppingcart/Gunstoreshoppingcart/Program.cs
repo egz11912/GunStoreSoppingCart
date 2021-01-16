25348b84d590761f5c36aafa00c1f9500a3d43a7
Eric Zuniga <egz11912@gmail.com>
2021-01-16 14:05:48 -0700

    Revised work

--- Gunstoreshoppingcart/Gunstoreshoppingcart/Program.cs
+++ Gunstoreshoppingcart/Gunstoreshoppingcart/Program.cs
@@ -1,24 +1,19 @@
 ﻿using System;
 
-namespace Gunstoreshoppingcart
+namespace GunStoreShoppingCart
 {
     class Program
     {
+        static void MyFunction()
+        {
+            Console.WriteLine("I am learning C#!");
+        }
+
         static void Main(string[] args)
         {
-            string firstName = "Eric ";
-            string lastName = "Zuniga";
-            string fullName = firstName + "" + lastName;
-            Console.WriteLine(fullName);
-
-            int x = 5;
-            int y = 6;
-            Console.WriteLine(x + y);
-
-            int sum1 = 10 + 5;        
-            int sum2 = sum1 + 100;      
-            int sum3 = sum2 + sum2;     
-            Console.WriteLine(sum3);
+            MyFunction();
+            MyFunction();
+            MyFunction();
         }
     }
 }
