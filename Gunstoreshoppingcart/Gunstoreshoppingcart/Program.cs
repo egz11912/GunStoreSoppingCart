80ad6969f9c2248a14382ca10d28878f7d1e0546
Eric Zuniga <egz11912@gmail.com>
2021-01-16 17:06:50 -0700

    New class= Instagram with users, roles, images, posts, and comments printed to the console

--- Gunstoreshoppingcart/Gunstoreshoppingcart/Program.cs
+++ Gunstoreshoppingcart/Gunstoreshoppingcart/Program.cs
@@ -2,18 +2,29 @@
 
 namespace GunStoreShoppingCart
 {
-    class Program
+    class InstagramPage
     {
-        static void MyFunction()
-        {
-            Console.WriteLine("I am learning C#!");
-        }
+        string users;
+        string roles;
+        string images;
+        string posts;
+        string comments;
 
         static void Main(string[] args)
         {
-            MyFunction();
-            MyFunction();
-            MyFunction();
+            InstagramPage page = new InstagramPage();
+            page.users = "Enduser";
+            page.roles = "actors";
+            page.images = "pictures";
+            page.posts = "new blog";
+            page.comments = "what do you think";
+
+
+            Console.WriteLine(page.comments);
+            Console.WriteLine(page.users);
+            Console.WriteLine(page.posts);
+
         }
     }
 }
+    
