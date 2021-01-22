c5a97bf0384f98e2ffb2a8799d72729b16eb8719
Eric Zuniga <egz11912@gmail.com>
2021-01-22 12:41:39 -0700

    Module 6 exercises

--- Gunstoreshoppingcart/Gunstoreshoppingcart/Program.cs
+++ Gunstoreshoppingcart/Gunstoreshoppingcart/Program.cs
@@ -1,26 +1,45 @@
 ﻿using System;
-using Gunstoreshoppingcart.Models;
+using System.Collections.Generic;
+namespace Gunstoreshoppingcart {
 
-namespace Gunstoreshoppingcart
+};
+
+class Program
 {
-    class Program
+    static void Main()
     {
-        public class SumOnetoTwentyNumbers
+
         {
-            public static void Main(string[] args)
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            
+            Console.WriteLine("Array elements are:");
+            foreach (int i in a)  
             {
-                int numStart = 1;
-                int numLast = 20;
-                int sum = 0;
-                while (numStart <= numLast)
-                {
-                    sum = sum + numStart;
-                    numStart++;
-                }
-                Console.Write("Sum of all Digits = " + sum);
+                Console.Write(i + "  ");   
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("List elements are:");
+            List<double> b = new List<double>();
+
+            b.Add(14.5);
+            b.Add(89.7);
+            b.Add(76.3);
+            b.Add(90.90);
+            b.Add(43.2);
+            b.Add(6.1);
+            b.Add(100.1);
+            b.Add(23.234);
+            b.Add(27.21);
+            b.Add(4.12);
+
+            for (int i = 0; i < b.Count; i++)
+            {
+                Console.Write(b[i] + ",   ");
             }
         }
     }
+
 }
-
-
