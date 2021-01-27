a329815384c7053da5e36a910619d960a5ac4ef0
Eric Zuniga <egz11912@gmail.com>
2021-01-27 11:56:45 -0700

    Yet another try at the mid term project

---
+++ Gunstoreshoppingcart/Gunstoreshoppingcart/EmptyClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Gunstoreshoppingcart;
+using Gunstoreshoppingcart.Models;
+using BakeryShoppingCart.DTOS;
+
+namespace Gunstoreshoppingcart
+{
+    public class Program
+
+    {
+        static void Main()
+        {
+            string response = "";
+
+            Users myUser = new Users();
+
+            while (response != "5")
+            {
+                myUser.PrintMenu();
+
+                response = myUser.CaptureResponse();
+
+                switch (response)
+                {
+                    case "1":
+                        myUser.CreateUser();
+                        break;
+                    case "2":
+                        myUser.CreateComment();
+                        break;
+                    case "3":
+                        myUser.ShowallUsers();
+                        break;
+                    case "4":
+                        myUser.ShowallComments();
+                        break;
+                    case "5":
+                        break;
+                    default:
+                        break;
+                        
+                }
+            }
+        }
+
+    }
+}
--- Gunstoreshoppingcart/Gunstoreshoppingcart/Models/public class Users.cs
+++ Gunstoreshoppingcart/Gunstoreshoppingcart/Models/public class Users.cs
@@ -10,6 +10,36 @@ namespace Gunstoreshoppingcart.Models
         public string SuperUser { get; set; }
         public int UserId { get; set; }
         public string Email { get; set; }
-     
+        public object ShowallComments { get; internal set; }
+
+        internal void PrintMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string CaptureResponse()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void CreateUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void CreateComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ShowallUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ShowallComments()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
